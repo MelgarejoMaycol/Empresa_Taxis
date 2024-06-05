097d419f6bcb45d922d21c0b4c389ad615d21944
Maycol Melgarejo <mfmelgarejo04@gmail.com>
2024-06-05 11:49:34 -0500

    Update AppConductores.jsx

--- Taxis/src/components/AppConductores.jsx
+++ Taxis/src/components/AppConductores.jsx
@@ -3,6 +3,7 @@ import {HeadConductores} from "./HeadConductores";
 import {ConductoresLista} from "./ConductoresLista";
 import '../../src/estilos/styles.css';
 import '../../src/estilos/Conductores.css';
+
 export const AppConductores = () => { 
   return (
    <>
